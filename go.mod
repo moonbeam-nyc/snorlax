8bbf5806a19bae8e47917bfe292b0134191528ae
Peter Valdez <ptr.vldz@gmail.com>
2024-03-20 02:36:38 -0400

    Add colorized cobra help text

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module moon-society/snorlax
 go 1.22.1
 
 require (
+	github.com/ivanpirog/coloredcobra v1.0.1
+	github.com/spf13/cobra v1.8.0
 	k8s.io/apimachinery v0.29.3
 	k8s.io/client-go v0.29.3
 )
@@ -10,6 +12,7 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/go-logr/logr v1.3.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
@@ -20,9 +23,12 @@ require (
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
--- main.go
+++ main.go
@@ -1,94 +1,111 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 
+	cc "github.com/ivanpirog/coloredcobra"
+
+	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "snorlax",
+	Short: "a service to that sleeps and wakes your containers",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Run the HTTP server",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Read environment variables
+		deploymentName := os.Getenv("DEPLOYMENT_NAME")
+		if deploymentName == "" {
+			log.Fatal("DEPLOYMENT_NAME environment variable is not set.")
+		}
+
+		replicaCountStr := os.Getenv("REPLICA_COUNT")
+		if replicaCountStr == "" {
+			log.Fatal("REPLICA_COUNT environment variable is not set.")
+		}
+
+		replicaCount, err := strconv.Atoi(replicaCountStr)
+		if err != nil {
+			log.Fatalf("Failed to convert REPLICA_COUNT to integer: %v", err)
+		}
+
+		// Setup config
+		var config *rest.Config
+		if kubeconfig := os.Getenv("KUBECONFIG"); kubeconfig != "" {
+			config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+			if err != nil {
+				log.Fatalf("Failed to create config from KUBECONFIG: %v", err)
+			}
+		} else {
+			config, err = rest.InClusterConfig()
+			if err != nil {
+				log.Fatalf("Failed to create in-cluster config: %v", err)
+			}
+		}
+
+		// Create the clientset
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			log.Fatalf("Failed to create clientset: %v", err)
+		}
+
+		// Define the HTTP handler function
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			// Scale the deployment
+			scale := fmt.Sprintf(`{"spec":{"replicas":%d}}`, replicaCount)
+			_, err = clientset.AppsV1().Deployments("default").Patch(r.Context(), deploymentName, types.StrategicMergePatchType, []byte(scale), metav1.PatchOptions{})
+			if err != nil {
+				log.Printf("Failed to scale deployment: %v", err)
+				http.Error(w, "Failed to scale deployment", http.StatusInternalServerError)
+				return
+			}
+
+			fmt.Fprintf(w, "Deployment scaled successfully to %d replicas", replicaCount)
+		})
+
+		// Start the web server
+		log.Println("Starting server on port 8080...")
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	},
+}
+
+var watchCmd = &cobra.Command{
+	Use:   "watch",
+	Short: "Print 'hello world'",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello, world!")
+	},
+}
+
 func main() {
-	// Read environment variables
-	deploymentName := os.Getenv("DEPLOYMENT_NAME")
-	if deploymentName == "" {
-		log.Fatal("DEPLOYMENT_NAME environment variable is not set.")
+    cc.Init(&cc.Config{
+        RootCmd:       rootCmd,
+        Headings:      cc.HiCyan + cc.Bold + cc.Underline,
+        Commands:      cc.HiYellow + cc.Bold,
+        Example:       cc.Italic,
+        ExecName:      cc.Bold,
+        Flags:         cc.Bold,
+    })
+
+	rootCmd.AddCommand(serveCmd, watchCmd)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-
-	replicaCountStr := os.Getenv("REPLICA_COUNT")
-	if replicaCountStr == "" {
-		log.Fatal("REPLICA_COUNT environment variable is not set.")
-	}
-
-	namespace := metav1.NamespaceDefault
-	if ns := os.Getenv("NAMESPACE"); ns != "" {
-		namespace = ns
-	}
-
-	replicaCount, err := strconv.Atoi(replicaCountStr)
-	if err != nil {
-		log.Fatalf("Failed to convert REPLICA_COUNT to integer: %v", err)
-	}
-
-    // Setup config
-    var config *rest.Config
-    if kubeconfig := os.Getenv("KUBECONFIG"); kubeconfig != "" {
-        config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
-        if err != nil {
-            log.Fatalf("Failed to create config from KUBECONFIG: %v", err)
-        }
-    } else {
-        config, err = rest.InClusterConfig()
-        if err != nil {
-            log.Fatalf("Failed to create in-cluster config: %v", err)
-        }
-    }
-
-	// Create the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Fatalf("Failed to create clientset: %v", err)
-	}
-
-	// Define the HTTP handler function
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Scale the deployment
-		// scale := fmt.Sprintf(`{"spec":{"replicas":%d}}`, replicaCount)
-		// _, err = clientset.AppsV1().Deployments("default").Patch(r.Context(), deploymentName, types.StrategicMergePatchType, []byte(scale), metav1.PatchOptions{})
-		// if err != nil {
-		// 	log.Printf("Failed to scale deployment: %v", err)
-		// 	http.Error(w, "Failed to scale deployment", http.StatusInternalServerError)
-		// 	return
-		// }
-
-		log.Printf("Scaling %s/%s to to %d replicas", namespace, deploymentName, replicaCount)
-
-		// Get the deployment
-		deployment, err := clientset.AppsV1().Deployments(namespace).Get(r.Context(), deploymentName, metav1.GetOptions{})
-		if err != nil {
-			log.Printf("Failed to get deployment: %v", err)
-			http.Error(w, "Failed to get deployment", http.StatusInternalServerError)
-			return
-		}
-
-		// Print the deployment JSON with indents
-		indentedJSON, err := json.MarshalIndent(deployment, "", "  ")
-		if err != nil {
-			log.Printf("Failed to marshal deployment to indented JSON: %v", err)
-			http.Error(w, "Failed to marshal deployment to indented JSON", http.StatusInternalServerError)
-			return
-		}
-		fmt.Fprintf(w, "%s", indentedJSON)
-	})
-
-
-	// Start the web server
-	log.Println("Starting server on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
