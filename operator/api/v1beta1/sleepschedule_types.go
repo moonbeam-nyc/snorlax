427ea8d9c12a5365a3e2f16be71aaa22d87a8214
Peter Valdez <ptr.vldz@gmail.com>
2024-05-12 17:16:42 -0400

    Add initial operator

---
+++ operator/api/v1beta1/sleepschedule_types.go
@@ -0,0 +1,73 @@
+/*
+Copyright 2024 Peter Valdez.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+type SleepScheduleSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	DeploymentName string    `json:"deploymentName,omitempty"`
+	Kubeconfig     string    `json:"kubeconfig,omitempty"`
+	Namespace      string    `json:"namespace,omitempty"`
+	ReplicaCount   int       `json:"replicaCount,omitempty"`
+	SleepTime      time.Time `json:"sleepTime,omitempty"`
+	WakeTime       time.Time `json:"wakeTime,omitempty"`
+	IngressName    string    `json:"ingressName,omitempty"`
+	Port           int       `json:"port,omitempty"`
+}
+
+// SleepScheduleStatus defines the observed state of SleepSchedule
+type SleepScheduleStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Awake    bool `json:"awake,omitempty"`
+	Replicas int  `json:"replicas,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// SleepSchedule is the Schema for the sleepschedules API
+type SleepSchedule struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SleepScheduleSpec   `json:"spec,omitempty"`
+	Status SleepScheduleStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// SleepScheduleList contains a list of SleepSchedule
+type SleepScheduleList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []SleepSchedule `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&SleepSchedule{}, &SleepScheduleList{})
+}
